f66901f26555cb49ebb632e07913c7c09bd02572
Zaki1726 <100398414+Zaki1726@users.noreply.github.com>
2022-09-12 06:34:41 +0700

    Update creator.js

--- plugins/creator.js
+++ plugins/creator.js
@@ -20,17 +20,17 @@ let handler  = async (m, { conn, command, args, usedPrefix, DevMode }) => {
 //------------ BIO
 let ppown = await conn.profilePictureUrl(nomorown + '@s.whatsapp.net', 'image').catch(_ => hwaifu[1]) 
 let teksbio = `${htki} *BIODATA* ${htka}
-${htjava} *💌 Nama* : WH-MODS-Dev
-${htjava} *✉️ Nama RL* : HAMBA ALLAH
-${htjava} *♂️ Gender* : Boys
+${htjava} *💌 Nama* : Zkhii!
+${htjava} *✉️ Nama RL* : ZhillBotz
+${htjava} *♂️ Gender* : laki - laki
 ${htjava} *🕋 Agama* : Islam
 ${htjava} *⏰ Tanggal lahir* : Private 🥶
-${htjava} *🎨 Umur* : 18
-${htjava} *🧮 Kelas* : SUDAH TAMAT
-${htjava} *🧩 Hobby* : MODIFIKASI WHATSAPP "MODDER WHATSAPP"
-${htjava} *💬 Sifat* : ASIK DAH KALAU DAH KENAL
-${htjava} *🗺️ Tinggal* : Indo, Depok, Jawa Barat
-${htjava} *❤️ Waifu* : -
+${htjava} *🎨 Umur* : 13
+${htjava} *🧮 Kelas* : 7
+${htjava} *🧩 Hobby* : Python,html,css,Bash scripting 
+${htjava} *💬 Sifat* : Asik,Happy
+${htjava} *🗺️ Tinggal* : Indo, Jawa barat,Karawang
+${htjava} *❤️ Waifu* : Elaina Wangy²
 
 ${htjava} *📷 ɪɴsᴛᴀɢʀᴀᴍ* : ${sig}
 ${htjava} *🐈 ɢɪᴛʜᴜʙ:* ${sgh}
